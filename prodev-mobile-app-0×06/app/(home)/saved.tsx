acd8b1a65942c4699b8bb00755f43f1a3760b7b3
vincent-eboya613 <vincentkidavi@gmail.com>
2025-09-13 13:47:38 +0300

    Create saved.tsx

---
+++ prodev-mobile-app-0×06/app/(home)/saved.tsx
@@ -0,0 +1,10 @@
+import {View, Text} from "react-native";
+
+const Saved = ()=> {
+    return(
+        <View>
+            <Text>Saved Screen</Text>
+            </View>
+    )
+}
+export default Saved;
